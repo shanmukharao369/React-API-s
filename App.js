0f1fe2cf9e145bc5414f984ced9fcc3dea0f0f3c
shanmukharao369 <115773678+shanmukharao369@users.noreply.github.com>
2023-02-06 13:04:17 +0530

    api calls task-6 ecommerce

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map(moviesData =>{
+      return {
+        id: moviesData.episode_id,
+        title: moviesData.title,
+        openingText: moviesData.opening_crawl,
+        releaseData: moviesData.release_date
+      };
+    })
+    setMovies(transformedMovies);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
